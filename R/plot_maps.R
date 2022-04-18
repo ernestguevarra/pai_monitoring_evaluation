84e18dc294bdca4b0695e5ea0ed0f08a26e8f06a
Ernest Guevarra <ernest@guevarra.io>
2022-04-18 19:38:35 +0000

    update actions workflow

--- R/plot_maps.R
+++ R/plot_maps.R
@@ -6,12 +6,12 @@
 #
 ################################################################################
 
-targets::tar_load(
-  c(lbr_counties, lbr_districts, lbr_clans, 
-    grand_bassa_ea, grand_bassa_sample_map, grand_bassa_sample,
-    urban_montserrado, urban_montserrado_ea, 
-    urban_montserrado_sample_map, urban_montserrado_sample)
-)
+# targets::tar_load(
+#   c(lbr_counties, lbr_districts, lbr_clans, 
+#     grand_bassa_ea, grand_bassa_sample_map, grand_bassa_sample,
+#     urban_montserrado, urban_montserrado_ea, 
+#     urban_montserrado_sample_map, urban_montserrado_sample)
+# )
 
 
 ################################################################################
@@ -22,18 +22,18 @@ targets::tar_load(
 #
 ################################################################################
 
-gb_districts <- lbr_districts |>
-  subset(admin1Name == "Grand Bassa")
-
-gb_map_spid <- grand_bassa_sample_map |>
-  dplyr::left_join(grand_bassa_sample |> 
-                     subset(select = c(spid, EFEACODE)), 
-                   by = "EFEACODE")
- 
-gm_map_spid <- urban_montserrado_sample_map |>
-  dplyr::left_join(urban_montserrado_sample |> 
-                     subset(select = c(spid, EFEACODE)), 
-                   by = "EFEACODE")
+# gb_districts <- lbr_districts |>
+#   subset(admin1Name == "Grand Bassa")
+# 
+# gb_map_spid <- grand_bassa_sample_map |>
+#   dplyr::left_join(grand_bassa_sample |> 
+#                      subset(select = c(spid, EFEACODE)), 
+#                    by = "EFEACODE")
+#  
+# gm_map_spid <- urban_montserrado_sample_map |>
+#   dplyr::left_join(urban_montserrado_sample |> 
+#                      subset(select = c(spid, EFEACODE)), 
+#                    by = "EFEACODE")
 
 ################################################################################
 #
@@ -43,38 +43,38 @@ gm_map_spid <- urban_montserrado_sample_map |>
 #
 ################################################################################
 
-png(
-  filename = "outputs/grand_bassa_sample_map.png",
-  width = 10, height = 10, units = "in",
-  res = 200
-)
-par(mar = c(0,0,0,0))
-plot(sf::st_geometry(grand_bassa_ea), lwd = 0.5, border = "gray70")
-plot(sf::st_geometry(gb_districts), lwd = 3, border = "gray50", add = TRUE)
-plot(sf::st_geometry(gb_map_spid), col = "gray90", add = TRUE)
-text(
-  as_Spatial(sf::st_centroid(gb_map_spid)), 
-  labels = gb_map_spid$spid, cex = 0.75, col = "red"
-)
-text(
-  as_Spatial(sf::st_centroid(gb_districts)), 
-  labels = gb_districts$admin2Name, cex = 1, col = "gray70"
-)
-dev.off()
-
-png(
-  filename = "outputs/urban_montserrado_sample_map.png",
-  width = 10, height = 10, units = "in",
-  res = 200
-)
-par(mar = c(0,0,0,0))
-plot(sf::st_geometry(urban_montserrado_ea), lwd = 0.5, border = "gray70")
-plot(sf::st_geometry(gm_map_spid), col = "gray90", add = TRUE)
-text(
-  as_Spatial(sf::st_centroid(gm_map_spid)), 
-  labels = gm_map_spid$spid, cex = 0.75, col = "red"
-)
-plot(sf::st_geometry(urban_montserrado), lwd = 1.5, add = TRUE)
-dev.off()
+# png(
+#   filename = "outputs/grand_bassa_sample_map.png",
+#   width = 10, height = 10, units = "in",
+#   res = 200
+# )
+# par(mar = c(0,0,0,0))
+# plot(sf::st_geometry(grand_bassa_ea), lwd = 0.5, border = "gray70")
+# plot(sf::st_geometry(gb_districts), lwd = 3, border = "gray50", add = TRUE)
+# plot(sf::st_geometry(gb_map_spid), col = "gray90", add = TRUE)
+# text(
+#   as_Spatial(sf::st_centroid(gb_map_spid)), 
+#   labels = gb_map_spid$spid, cex = 0.75, col = "red"
+# )
+# text(
+#   as_Spatial(sf::st_centroid(gb_districts)), 
+#   labels = gb_districts$admin2Name, cex = 1, col = "gray70"
+# )
+# dev.off()
+# 
+# png(
+#   filename = "outputs/urban_montserrado_sample_map.png",
+#   width = 10, height = 10, units = "in",
+#   res = 200
+# )
+# par(mar = c(0,0,0,0))
+# plot(sf::st_geometry(urban_montserrado_ea), lwd = 0.5, border = "gray70")
+# plot(sf::st_geometry(gm_map_spid), col = "gray90", add = TRUE)
+# text(
+#   as_Spatial(sf::st_centroid(gm_map_spid)), 
+#   labels = gm_map_spid$spid, cex = 0.75, col = "red"
+# )
+# plot(sf::st_geometry(urban_montserrado), lwd = 1.5, add = TRUE)
+# dev.off()
 
 
