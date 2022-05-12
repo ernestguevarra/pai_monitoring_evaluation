fe784978e898517015394c42236efd60d53fdc11
Ernest Guevarra <ernest@guevarra.io>
2022-05-12 07:52:20 +0100

    edit raw_data targets

--- _targets.R
+++ _targets.R
@@ -137,7 +137,10 @@ data_raw <- tar_plan(
   tar_target(
     name = raw_data,
     command = get_kobo_data(form_id = raw_data_id) |>
-      subset(as.Date(today) >= as.Date("2022-05-10")),
+      subset(as.Date(today) >= as.Date("2022-05-10")) |>
+      dplyr::mutate(
+        eid = stringr::str_pad(eid, width = 10, side = "left", pad = "0")
+      ),
     cue = tar_cue(mode = "always")
   )#,
   #raw_data = get_kobo_data(form_id = raw_data_id) |>
