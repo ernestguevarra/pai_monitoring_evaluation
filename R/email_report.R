3d1aa25b5e3824d34597975d7e6db3d3e1444375
Ernest Guevarra <ernest@guevarra.io>
2022-08-29 11:36:20 +0100

    add email deploy to gdrive workflow step

--- R/email_report.R
+++ R/email_report.R
@@ -86,3 +86,42 @@ email_quality_report <- function(message = email_report_message,
       )
     )  
 }
+
+
+################################################################################
+#
+#'
+#' Email report to recipeients
+#' 
+#' @param message Email message to send
+#' @param attachment The filename for the file to be attached to the email. 
+#'   Default to "outputs/data_review.html"
+#' @param sender Email address of the sender of the email. Default to
+#'   Sys.getenv("GMAIL_USERNAME")
+#' @param recipient Email addresses of report recipient/s. Default to
+#'   Sys.getenv("REPORT_RECIPIENTS")
+#' 
+#
+################################################################################
+
+email_coverage_dashboard <- function(message = email_dashboard_message,
+                                     #attachment = survey_progress_report[1],
+                                     sender = Sys.getenv("GMAIL_USERNAME"),
+                                     recipient = Sys.getenv("REPORT_RECIPIENTS")) {
+  blastula::smtp_send(
+    email = message,
+    to = recipient,
+    bcc = sender,
+    from = sender, 
+    subject = paste0(
+      "CMAM Coverage Dashboard - ", Sys.Date()
+    ), 
+    credentials = blastula::creds_envvar(
+      user = sender, 
+      pass_envvar = "GMAIL_PASSWORD", 
+      host = "smtp.gmail.com", 
+      port = 465,
+      use_ssl = TRUE
+    )
+  )  
+}
